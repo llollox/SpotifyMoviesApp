27984a11efe94358fbeb8c1f7edf8a351f1a6ff6
llollox <lore91tanz@gmail.com>
2019-12-02 20:22:07 +0100

    Add fail test on base module

--- base/src/test/java/com/lorenzorigato/base/ExampleUnitTest.java
+++ base/src/test/java/com/lorenzorigato/base/ExampleUnitTest.java
@@ -14,4 +14,9 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void fail_addition_isCorrect() {
+        assertEquals(4, 2 + 1);
+    }
 }
