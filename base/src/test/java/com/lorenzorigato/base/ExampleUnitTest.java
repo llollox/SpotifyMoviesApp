e7edad8ef19073528173cbda64377b0b478949c9
llollox <lore91tanz@gmail.com>
2019-12-02 20:24:48 +0100

    Remove failed test on base module

--- base/src/test/java/com/lorenzorigato/base/ExampleUnitTest.java
+++ base/src/test/java/com/lorenzorigato/base/ExampleUnitTest.java
@@ -14,9 +14,4 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
-
-    @Test
-    public void fail_addition_isCorrect() {
-        assertEquals(4, 2 + 1);
-    }
 }
