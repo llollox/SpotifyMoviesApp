d15c4733fff7ad0a46e8bca33efcb451b12ab83e
llollox <lore91tanz@gmail.com>
2019-12-07 15:04:55 +0100

    Add GradientTransformation. Fix actor layout

--- movies/src/main/java/com/lorenzorigato/movies/ui/component/DataBindingAdapter.java
+++ movies/src/main/java/com/lorenzorigato/movies/ui/component/DataBindingAdapter.java
@@ -4,6 +4,7 @@ import android.widget.ImageView;
 
 import androidx.databinding.BindingAdapter;
 
+import com.lorenzorigato.base.components.image.transformation.GradientTransformation;
 import com.lorenzorigato.movies.R;
 import com.squareup.picasso.Picasso;
 
@@ -12,5 +13,14 @@ public class DataBindingAdapter {
     @BindingAdapter("url")
     public static void loadImage(ImageView imageView, String url) {
         Picasso.get().load(url).placeholder(R.drawable.loading).into(imageView);
+    }
+
+    @BindingAdapter(value = "urlWithBottomGradient")
+    public static void loadImageWithBottomGradient(ImageView imageView, String url) {
+        Picasso.get()
+                .load(url)
+                .placeholder(R.drawable.loading)
+                .transform(new GradientTransformation())
+                .into(imageView);
     }
 }
