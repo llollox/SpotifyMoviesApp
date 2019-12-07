5c0680cb5a8553e69d6cc9bea2223e6d370820a8
llollox <lore91tanz@gmail.com>
2019-12-07 14:24:23 +0100

    Fix AndroidManifest. Fix movie detail activity layout

--- movies-sample/src/main/java/com/lorenzorigato/movies_sample/ui/TestActivity.java
+++ movies-sample/src/main/java/com/lorenzorigato/movies_sample/ui/TestActivity.java
@@ -13,6 +13,10 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.startDetailActivity();
+    }
+
+    private void startDetailActivity() {
         Intent intent = MovieDetailActivity.getCallingIntent(this, 682);
         startActivity(intent);
         finish();
